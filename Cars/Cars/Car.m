91cd095aaf56e9013ed907ad0d32dd9e3eb03b32
sofiaknezevic <sofia.knezevic@gmail.com>
2017-01-30 11:46:02 -0800

    Add files via upload

---
+++ Cars/Cars/Car.m
@@ -0,0 +1,28 @@
+//
+//  Car.m
+//  Cars
+//
+//  Created by Sofia Knezevic on 2017-01-19.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+-(void) drive {
+    
+    NSLog(@"You are driving a: %@", _model);
+}
+
+-(id) initWithModel:(NSString *)model {
+    
+    if(self = [super init]){
+        _model = model;
+    }
+    
+    return self;
+    
+}
+
+@end
---
+++ Cars/Cars/Toyota.m
@@ -0,0 +1,22 @@
+//
+//  Toyota.m
+//  Cars
+//
+//  Created by Sofia Knezevic on 2017-01-22.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+#import "Toyota.h"
+
+@implementation Toyota
+
+-(id) init{
+    
+    if(self = [super init]){
+        [self setModel: @"Prius"];
+    }
+    
+    return self;
+}
+
+@end
